5ea2f52ced1bbd0234a5ffa4de36e99ef3675da7
Hantsi Taumang <63910830+hanSiey@users.noreply.github.com>
2025-11-23 00:45:41 +0200

    prj name script fix

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
